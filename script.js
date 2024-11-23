8f9d073812d287a1cae1afe90b6a49e87cabf0cd
Luiz Gustavo Oliveira <121184767+Guh698@users.noreply.github.com>
2024-11-23 15:26:31 -0300

    Update script.js
    Tabela 2 para quantidade

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const inputBox = document.getElementById("input-box");
+const inputBox2 = document.getElementById("input-box2");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
   if (inputBox.value === "") {
     alert("É necessário que insira nome à tarefa");
+  } else if (inputBox2.value === "") {
+    alert("É necessário que insira valor ao produto");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+    li.innerHTML = `${inputBox.value} - Quantidade: ${inputBox2.value}`;
     listContainer.appendChild(li);
+
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
   }
 
   inputBox.value = "";
+  saveData();
+  inputBox2.value = "";
   saveData();
 }
 
